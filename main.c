44cffad8b1669ac5e41d91f3c6fb1afb27e91ca8
DogeSSBM <dogessbm@gmail.com>
2021-06-18 00:41:35 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -27,17 +27,52 @@ Lisc* parsc(Lisc *trec, const char* inpuc)
 	return trec;
 }
 
+/*
++ 2 4 5 (+ 3 5 1)
+
+`(1 2 3 ,@(a b c) 4 5) => (1 2 3 a b c 4 5)
+`(1 2 gensym 4) => (1 2 gensym 4)
+(define gensym 3)
+`(1 2 ,gensym 4) => (1 2 3 4)
+*/
+
 
 Lisc *add(Lisc *thingec)
 {
-	return calloc(1, sizeof(Lisc));
+	int accumulator = 0;
+	Lisc *retc = calloc(1, sizeof(Lisc));
+	if(!thingec) return retc;
 
+	do{
+		if(thingec->typc == lisc_t){
+			if(thingec->lisc->typc != func_t){
+				fprintf(stderr, "Invalid function in nested list\n");
+				return retc;
+			}
+			Lisc *ret = thingec->lisc->func(thingec->lisc->nexc);
+			ret.nexc = thingec->nexc;
+			thingec->nexc = ret;
+		}else if(thingec->typc == inc_t){
+			accumulator += thingec->inc;
+			thingec = thingec->nexc;
+		}else if(thingec->typc == err_t){
+			return retc;
+		}else{
+			fprintf(stderr, "Non numeric atoms in input\n");
+			return retc;
+		}
+	}while(thingec->next != NULL);
+
+	retc->typc = inc_t;
+	retc->inc = accumulator;
+	return retc;
 }
 
 LiscFunc func = add;
 
 Lisc *mul(Lisc *thingec)
 {
+
 	return calloc(1, sizeof(Lisc));
 }
 
